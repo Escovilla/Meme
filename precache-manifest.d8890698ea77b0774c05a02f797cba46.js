56d5d3bb144039db36e8de8e87a5398d06d239d5
Nico Escovilla <escovillanico@gmail.com>
2020-06-25 09:54:38 +0800

    nani des

---
+++ precache-manifest.d8890698ea77b0774c05a02f797cba46.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84e89a7ff036f8041c198085bdb3da2",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3e12ab23cf8b9c203eb2",
+    "url": "/static/css/main.9160cc98.chunk.css"
+  },
+  {
+    "revision": "431e819d44c52845c095",
+    "url": "/static/js/2.67f119d0.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.67f119d0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3e12ab23cf8b9c203eb2",
+    "url": "/static/js/main.715c11a7.chunk.js"
+  },
+  {
+    "revision": "3ee6d9289089d700c3ad",
+    "url": "/static/js/runtime-main.2d4e65bc.js"
+  },
+  {
+    "revision": "6e5d6096c02e8c18ad95d40ae5bf0089",
+    "url": "/static/media/loading-png-gif.6e5d6096.gif"
+  }
+]);
